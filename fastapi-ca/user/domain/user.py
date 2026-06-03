f13f27409aa930adb5e691f3fbd15d33edd71707
hjw3417 <rapheo0913@gmail.com>
2026-06-03 12:58:07 +0900

    feat: User class Profile 클래스로 name, email field 분리

--- fastapi-ca/user/domain/user.py
+++ fastapi-ca/user/domain/user.py
@@ -1,12 +1,15 @@
 from dataclasses import dataclass
 from datetime import datetime
 
+@dataclass
+class Profile:
+    name: str
+    email: str
 
 @dataclass
 class User:
     id: str
-    name: str
-    email: str
+    profile: Profile
     password: str
     created_at: datetime
     updated_at: datetime
