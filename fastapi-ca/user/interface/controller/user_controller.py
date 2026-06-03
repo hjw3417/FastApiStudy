5cf112f3045ea2483099c0915f390f0212dd47d6
hjw3417 <rapheo0913@gmail.com>
2026-06-03 15:04:16 +0900

    feat: 유저 생성 유스케이스 호출

--- fastapi-ca/user/interface/controller/user_controller.py
+++ fastapi-ca/user/interface/controller/user_controller.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 
+from user.application.user_service import UserService
+
 class UserCreate(BaseModel):
     name: str
     email: str
@@ -10,4 +12,8 @@ router = APIRouter(prefix="/users")
 
 @router.post("", status_code=201)
 def create_user(user: UserCreate):
+    user_service = UserService()
+    created_user = user_service.create_user(user.name, user.email, user.password)
     return user
+
+
