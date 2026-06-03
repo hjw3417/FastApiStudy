bf84297893061d6182759ebd5f148f29233fe5ec
hjw3417 <rapheo0913@gmail.com>
2026-06-03 15:58:10 +0900

    feat: database 명 - -> _ 로 정석 snake case로 변경

--- fastapi-ca/database.py
+++ fastapi-ca/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test@localhost:3306/fastapi-ca"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test@localhost:3306/fastapi_ca"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
