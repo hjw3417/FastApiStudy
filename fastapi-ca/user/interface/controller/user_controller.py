d36a0c9591ce6869cd17acdbf22b3db27bfc13c3
hjw3417 <rapheo0913@gmail.com>
2026-06-03 14:39:17 +0900

    feat: pydantic 으로 유효성 검사 추가

--- fastapi-ca/user/interface/controller/user_controller.py
+++ fastapi-ca/user/interface/controller/user_controller.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+    password: str
 
 router = APIRouter(prefix="/users")
 
 @router.post("", status_code=201)
-def create_user():
-    return "user created"
+def create_user(user: UserCreate):
+    return user
