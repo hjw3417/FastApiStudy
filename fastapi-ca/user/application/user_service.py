51eac09b4e1d7672a23230c41d3081c2226986fd
hjw3417 <rapheo0913@gmail.com>
2026-06-06 16:51:05 +0900

    feat: login 시 jwt 반환하도록 수정

--- fastapi-ca/user/application/user_service.py
+++ fastapi-ca/user/application/user_service.py
@@ -3,11 +3,12 @@ from datetime import datetime
 from user.domain.user import User
 from user.domain.repository.user_repo import IUserRepository
 from user.infra.repository.user_repo import UserRepository
-from fastapi import HTTPException, Depends
+from fastapi import HTTPException, Depends, status
 from utils.crypto import Crypto
 from typing import Annotated
 from dependency_injector.wiring import inject, Provide
 from fastapi import Depends
+from common.auth import create_access_token
 
 
 class UserService:
@@ -70,3 +71,15 @@ class UserService:
     
     def delete_user(self, user_id:str):
         self.user_repo.delete(user_id)
+
+    def login(self, email:str, password: str):
+        user = self.user_repo.find_by_email(email)
+
+        if not self.crypto.verify(password, user.password):
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        
+        access_token = create_access_token(
+            payload={"user_id":user.id}
+        )
+
+        return access_token
