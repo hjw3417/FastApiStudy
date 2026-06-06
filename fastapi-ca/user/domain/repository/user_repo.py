135adf39202399da4cfe8a0990898e2504d65027
hjw3417 <rapheo0913@gmail.com>
2026-06-06 13:00:07 +0900

    feat: 유저 검색 후 update 기능 추가

--- fastapi-ca/user/domain/repository/user_repo.py
+++ fastapi-ca/user/domain/repository/user_repo.py
@@ -13,4 +13,7 @@ class IUserRepository(metaclass=ABCMeta):
         검색한 유저가 없을 경우 422 에러를 발생시킨다.
         """
         raise NotImplementedError
+    @abstractmethod
+    def update(self, user: User):
+        raise NotImplementedError
         
--- fastapi-ca/user/interface/controller/user_controller.py
+++ fastapi-ca/user/interface/controller/user_controller.py
@@ -23,3 +23,19 @@ def create_user(
     return created_user
 
 
+class UserUpdate(BaseModel):
+    name: str | None = None
+    password: str | None = None
+
+    @router.put("/{user_id}")
+    @inject
+    def update_user(
+        user_id: str,
+        user: UserUpdate,
+        user_service: UserService = Depends(Provide[Container.user_service])
+    ):
+        user_service.update_user(
+            user_id=user_id,
+            name=user.name,
+            password=user.password
+        )
