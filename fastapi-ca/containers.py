8e7a580b06bd5a5ac4d43e05ee70b40fa07535cd
hjw3417 <rapheo0913@gmail.com>
2026-06-06 12:01:14 +0900

    fix: 오타 수정

--- fastapi-ca/containers.py
+++ fastapi-ca/containers.py
@@ -1,7 +1,7 @@
 from dependency_injector import containers, providers
-from user.infra.repositor.user_repo import UserRepository
+from user.infra.repository.user_repo import UserRepository
 
-class container(containers.DeclarativeContainer):
+class Container(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(
         packages=["user"]
     )
