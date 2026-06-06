576fae17ebc49e3b9d1759bc8e6e56b8edd61ef2
hjw3417 <rapheo0913@gmail.com>
2026-06-06 16:54:48 +0900

    feat: 토큰에 역할 추가

--- fastapi-ca/common/auth.py
+++ fastapi-ca/common/auth.py
@@ -1,16 +1,27 @@
 from datetime import datetime, timedelta
 from fastapi import HTTPException, status
 from jose import JWTError, jwt
+from enum import StrEnum
 
 SECRET_KEY = "THIS_IS_SUPER_SECRET_KEY"
 ALGORITHM = "HS256"
 
+class Role(StrEnum):
+    ADMIN = "ADMIN"
+    USER = "USER"
+
 def create_access_token(
         payload: dict,
+        role: Role,
         expires_delta:  timedelta = timedelta(hours=6),
 ):
     expire = datetime.utcnow() + expires_delta
-    payload.update({"exp": expire})
+    payload.update(
+        {
+            "role": role,
+            "exp": expire
+            }
+    )
     encode_jwt = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
     return encode_jwt
