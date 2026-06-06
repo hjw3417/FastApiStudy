60e751932feb31150a7fb4f6f158daf1787c93d3
hjw3417 <rapheo0913@gmail.com>
2026-06-06 16:23:51 +0900

    feat: auth.py 마저 작성

--- fastapi-ca/common/auth.py
+++ fastapi-ca/common/auth.py
@@ -4,3 +4,19 @@ from jose import JWTError, jwt
 
 SECRET_KEY = "THIS_IS_SUPER_SECRET_KEY"
 ALGORITHM = "HS256"
+
+def create_access_token(
+        payload: dict,
+        expires_delta:  timedelta = timedelta(hours=6),
+):
+    expire = datetime.utcnow() + expires_delta
+    payload.update({"exp": expire})
+    encode_jwt = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encode_jwt
+
+def decode_access_token(token: str):
+    try:
+        return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except JWTError:
+        raise HTTPException(status_code= status.HTTP_401_UNAUTHORIZED)
