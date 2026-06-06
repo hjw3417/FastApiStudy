278af9c49fa1420efac4ea02022df2c3af7c2dc6
hjw3417 <rapheo0913@gmail.com>
2026-06-06 14:43:52 +0900

    feat: create_user 응답 형태 추가

--- fastapi-ca/user/interface/controller/user_controller.py
+++ fastapi-ca/user/interface/controller/user_controller.py
@@ -1,15 +1,26 @@
 from fastapi import APIRouter, Depends
-from pydantic import BaseModel
-from typing import Annotated
 from dependency_injector.wiring import inject, Provide
 from containers import Container
 from user.application.user_service import UserService
 from pydantic import BaseModel, EmailStr, Field
+from datetime import datetime
 
 class UserCreate(BaseModel):
     name: str = Field(min_length=2, max_length=32)
     email: EmailStr = Field(max_length=64)
     password: str = Field(min_length=8, max_length=32)
+
+class UserUpdate(BaseModel):
+    name: str | None = None
+    password: str | None = None
+
+class UserResponse(BaseModel):
+    id:str
+    name: str
+    email: str
+    create_at: datetime
+    update_at: datetime
+
 
 router = APIRouter(prefix="/users")
 
@@ -19,14 +30,12 @@ def create_user(
     user: UserCreate,
     user_service: UserService = Depends(Provide[Container.user_service])
     # user_service: UserService = Depends(Provide["user_service"])
-):
+)->UserResponse:
     created_user = user_service.create_user(user.name, user.email, user.password)
     return created_user
 
 
-class UserUpdate(BaseModel):
-    name: str | None = None
-    password: str | None = None
+
 
 @router.put("/{user_id}")
 @inject
